466c948a1de62f209d45c91533b9c8291c69d423
VinceValiente <valiente.vinceellydit2011@gmail.com>
2024-10-28 08:15:15 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/components/Navbar/index.js

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Nav, NavLink, NavbarContainer, Span, NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileNavLogo, MobileLink } from './NavbarStyledComponent'
-import { DiCssdeck } from 'react-icons/di';
+import { Check as Check } from 'react-icons/di';
 import { FaBars } from 'react-icons/fa';
 import { Bio } from '../../data/constants';
 import { useTheme } from 'styled-components';
@@ -14,7 +14,7 @@ const Navbar = () => {
         <NavLogo to='/'>
           <a style={
             { display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }            }>
-            <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+            <Check size="3rem" /> <Span>Portfolio</Span>
           </a>
         </NavLogo>
         <MobileIcon>
